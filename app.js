b8e5924deee4675a14b5bc224016800ed2df49d3
stephaniesomade <stephaniesomadee@gmail.com>
2022-05-03 12:48:07 +0100

    MVP Routing

--- app.js
+++ app.js
@@ -2,9 +2,19 @@ const express = require("express");
 // const session = require("express-session");
 // const mongoose = require('mongoose');
 const app = express();
+const path = require("path");
+
 
 // Parses the response
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+
+const HomeRouter = require("./routes/home");
+
+app.use("/", HomeRouter);
 
 app.listen(3000, () => console.log("Server is listening on port 3000"))
---
+++ controllers/home.js
@@ -0,0 +1,7 @@
+const HomeController = {
+  Index: (req, res) => {
+     res.render("home/index");
+  }
+}
+
+module.exports = HomeController;

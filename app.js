104c6920c94117b042cbf231c2bc077d88581fdf
stephaniesomade <stephaniesomadee@gmail.com>
2022-05-03 11:37:17 +0100

    Downloaded key dependencies

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const session = require("express-session");
+const mongoose = require('mongoose');
+const app = express();
+
+// Parses the response
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(3000, () => console.log("Server is listening on port 3000"))
+

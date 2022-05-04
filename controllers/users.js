61f77d0946069bbb845ffd42dd6356d2929055e7
stephaniesomade <stephaniesomadee@gmail.com>
2022-05-04 14:32:07 +0100

    Profile details added

--- controllers/users.js
+++ controllers/users.js
@@ -13,6 +13,10 @@ const UsersController = {
       }
       res.status(201).redirect("/");
     });
+  }, 
+  Profile: (req, res) => { 
+    console.log(req.session.user.name)
+    res.render("users/profile", {name: req.session.user.name, email: req.session.user.email})
   }
 }
 

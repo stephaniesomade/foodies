2e74b11181449c65b4bb56e5706eef6427237de9
stephaniesomade <stephaniesomadee@gmail.com>
2022-05-04 16:13:44 +0100

    Editing details

--- routes/users.js
+++ routes/users.js
@@ -6,5 +6,7 @@ const UsersController = require('../controllers/users')
 router.get('/new', UsersController.New)
 router.post('/', UsersController.Create)
 router.get('/profile', UsersController.Profile)
+router.post('/name', UsersController.Name)
+router.post('/email', UsersController.Email)
 
 module.exports = router;
